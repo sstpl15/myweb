2cb7e30e359831a93ae187713fda2f7385e38ab8
safquat <safquat.hussain@sstpl.in>
2024-02-24 15:06:28 +0530

    4

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Path to your private key
-PRIVATE_KEY="$(dirname "$0")/tolearn.pem"
-
 # Path to your Docker Compose file
 DOCKER_COMPOSE_FILE="$(dirname "$0")/docker-compose.yml"
 
+# Decrypt the private key
+echo "${{ secrets.SSH_PRIVATE_KEY }}" > key.pem.enc
+openssl aes-256-cbc -d -a -in key.pem.enc -out key.pem -pass env:SSH_PASSPHRASE
+
+# Set appropriate permissions for the key
+chmod 600 key.pem
+
 # SSH into your EC2 instance
-ssh -i "$PRIVATE_KEY" ubuntu@13.201.229.180 << 'ENDSSH'
+ssh -i "key.pem" ubuntu@13.201.229.180 << 'ENDSSH'
+  # Commands to execute on the remote server
   # Pull the latest Docker image
   docker pull sstpl/myweb:latest
   
